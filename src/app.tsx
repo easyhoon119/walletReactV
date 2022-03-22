d379e16fb19bbbb1de19841d1aa2132807eab48d
easyhoon119forour <99165247+easyhoon119forour@users.noreply.github.com>
2022-03-22 10:54:36 +0900

    webpack settting

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import './app.css';
+
+function App() {
+    return (
+        <div className="hihi">
+            <p>hihih</p>
+            <p>im A</p>
+        </div>
+    );
+}
+
+export default App;
---
+++ webpack.common.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isProduction = process.env.NODE_ENV === 'PRODUCTION';
+
+module.exports = {
+    entry : './src/index.tsx',
+    output : {
+        path : path.resolve(__dirname,'dist'),
+        filename : '[name].[chunkhash].js'
+    },
+    resolve : {
+        extensions : [".js", ".jsx",".ts", ".tsx"]
+    }
+    ,
+    module : {
+        rules : [
+            {
+            test : /\.css$/i,
+            use : [
+                {
+                    loader : MiniCssExtractPlugin.loader
+                }
+                ,
+                {
+                    loader : 'css-loader',
+                    options : {
+                        modules : true
+                    }
+                },
+                {
+                    loader : 'postcss-loader',
+                }
+            ]
+        }, 
+        {
+            test : /\.(woff|woff2|eot|ttf|otf)$/i,
+            use : [{
+                loader : 'file-loader',
+                options : {
+                    name : '[contenthash].[ext]',
+                    outputPath : 'assets/fonts/'
+                }
+            }]
+        },
+        {
+            test : /\.(png|jpg|jpeg|gif|svg|webp)$/i,
+            use : [{
+                loader : 'url-loader',
+                options : {
+                    limit : 5000,
+                    fallback : 'file-loader',
+                    outputPath : 'assets/images/'
+                }
+            }]
+        },
+        {
+            test : /\.tsx?$/,
+            exclude : /node_modules/,
+            use : ["babel-loader","ts-loader"],
+        }
+    ]
+    },
+    plugins : [
+        new MiniCssExtractPlugin({
+            filename : '[contenthash].css'
+        }),
+        new webpack.ProvidePlugin({
+            React : 'react'
+        })
+        ,
+        new HtmlWebpackPlugin({
+            template : './public/index.html',
+            minify: isProduction ? {
+                collapseWhitespace: true,
+                useShortDoctype: true,
+                removeScriptTypeAttributes: true
+            } : false
+        }),
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({
+            IS_PRODUCTION: isProduction
+        })
+    ],
+}
